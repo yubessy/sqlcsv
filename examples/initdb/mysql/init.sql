93d30f3b8fe8fc8767985796e1acb3381fe44bff
yubessy <yubessy0@gmail.com>
2018-11-23 19:41:58 +0900

    remove blob type support

--- examples/initdb/mysql/init.sql
+++ examples/initdb/mysql/init.sql
@@ -3,15 +3,14 @@ CREATE TABLE testtable (
   int_col INT,
   float_col FLOAT,
   varchar_col VARCHAR(255),
-  blob_col BLOB,
   datetime_col DATETIME
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 ;
 
 INSERT INTO
-  testtable(int_col, float_col, varchar_col, blob_col, datetime_col)
+  testtable(int_col, float_col, varchar_col, datetime_col)
 VALUES
-  (1, 1.0, 'aaa', 'xxx', '2001-01-01 01:23:45'),
-  (2, 2.0, 'bbb', 'yyy', '2002-02-02 12:34:56'),
-  (NULL, NULL, NULL, NULL, NULL)
+  (1, 1.0, 'aaa', '2001-01-01 01:23:45'),
+  (2, 2.0, 'bbb', '2002-02-02 12:34:56'),
+  (NULL, NULL, NULL, NULL)
 ;
--- examples/initdb/postgres/init.sql
+++ examples/initdb/postgres/init.sql
@@ -4,15 +4,14 @@ CREATE TABLE testschema.testtable (
   int_col INT,
   real_col REAL,
   varchar_col VARCHAR(255),
-  bytea_col BYTEA,
   timestamp_col TIMESTAMP
 )
 ;
 
 INSERT INTO
-  testschema.testtable(int_col, real_col, varchar_col, bytea_col, timestamp_col)
+  testschema.testtable(int_col, real_col, varchar_col, timestamp_col)
 VALUES
-  (1, 1.0, 'aaa', 'xxx', '2001-01-01 01:23:45'),
-  (2, 2.0, 'bbb', 'yyy', '2002-02-02 12:34:56'),
-  (NULL, NULL, NULL, NULL, NULL)
+  (1, 1.0, 'aaa', '2001-01-01 01:23:45'),
+  (2, 2.0, 'bbb', '2002-02-02 12:34:56'),
+  (NULL, NULL, NULL, NULL)
 ;
