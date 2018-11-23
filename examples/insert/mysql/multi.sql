411818d64402758fc5384996e079f16d044f73ad
yubessy <yubessy0@gmail.com>
2018-11-23 20:58:11 +0900

    add test data

---
+++ examples/insert/mysql/multi.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+  testtable(int_col, float_col, varchar_col, datetime_col)
+VALUES
+  (%s, %s, %s, %s)
+;
---
+++ examples/insert/postgres/multi.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+  testschema.testtable(int_col, real_col, varchar_col, timestamp_col)
+VALUES
+  (%s, %s, %s, %s)
+;
