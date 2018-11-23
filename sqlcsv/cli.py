14608ac17de30af633bbb8f5405d240481ef0a86
yubessy <yubessy0@gmail.com>
2018-11-23 23:10:00 +0900

    modify cli

--- sqlcsv/cli.py
+++ sqlcsv/cli.py
@@ -8,23 +8,33 @@ from .casting import TypeCaster
 
 @click.group()
 @click.option('--db-url', type=str, required=True)
+@click.option('--header/--no-header', default=True)
+@click.option('--delimiter', type=str, default=',')
 @click.pass_context
-def cli(ctx, db_url):
-    ctx.obj['engine'] = create_engine(db_url)
+def cli(ctx, db_url, header, delimiter):
+    ctx.obj['db-url'] = db_url
+    ctx.obj['header'] = header
+    ctx.obj['delimiter'] = delimiter
 
 
 @cli.command()
-@click.argument('sql-file', type=click.File('r'))
-@click.argument('out-file', type=click.File('w'))
+@click.option('--lineterminator', type=str, default='\n')
+@click.argument('sqlfile', type=click.File('r'))
+@click.argument('csvfile', type=click.File('w'))
 @click.pass_context
-def select(ctx, sql_file, out_file):
-    sql = sql_file.read()
-    sql_file.close()
-    writer = csv.writer(out_file, lineterminator='\n')
+def select(ctx, lineterminator, sqlfile, csvfile):
+    sql = sqlfile.read()
+    sqlfile.close()
+    writer = csv.writer(
+        csvfile=csvfile,
+        delimiter=ctx.obj['delimiter'],
+        lineterminator=lineterminator,
+    )
+    engine = create_engine(ctx.obj['db-url'])
 
-    engine = ctx.obj['engine']
     result = engine.execute(sql)
-    writer.writerow(result.keys())
+    if ctx.obj['header']:
+        writer.writerow(result.keys())
     for row in result:
         writer.writerow(row)
 
@@ -33,18 +43,22 @@ def select(ctx, sql_file, out_file):
 @click.option('--types', type=str, required=True)
 @click.option('--nullables', type=str, default=None)
 @click.option('--date-format', type=str, default='%Y-%m-%d %H:%M:%S')
-@click.argument('sql-file', type=click.File('r'))
-@click.argument('in-file', type=click.File('r'))
+@click.argument('sqlfile', type=click.File('r'))
+@click.argument('csvfile', type=click.File('r'))
 @click.pass_context
-def insert(ctx, types, nullables, date_format, sql_file, in_file):
-    sql = sql_file.read()
-    sql_file.close()
-    reader = csv.reader(in_file)
+def insert(ctx, types, nullables, date_format, sqlfile, csvfile):
+    sql = sqlfile.read()
+    sqlfile.close()
+    reader = csv.reader(
+        csvfile=csvfile,
+        delimiter=ctx.obj['delimiter'],
+    )
+    engine = create_engine(ctx.obj['db-url'])
 
     caster = TypeCaster(types, nullables, date_format)
 
-    engine = ctx.obj['engine']
-    next(reader)
+    if ctx.obj['header']:
+        next(reader)
     engine.execute(sql, *(caster.cast(row) for row in reader))
 
 
