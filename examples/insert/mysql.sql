aaba41971512b2a910387c4bbb83c4f10b37742d
yubessy <yubessy0@gmail.com>
2018-11-24 01:18:07 +0900

    refactor examples

---
+++ examples/insert/mysql.sql
@@ -0,0 +1,3 @@
+INSERT INTO testtable(int_col, float_col, varchar_col, datetime_col)
+VALUES (%s, %s, %s, %s)
+;
--- examples/insert/mysql/multi.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO
-  testtable(int_col, float_col, varchar_col, datetime_col)
-VALUES
-  (%s, %s, %s, %s)
-;
