085c8fcad0b72329bc7bb1abc44b9a1f22ffcbac
yubessy <yubessy0@gmail.com>
2018-11-25 20:41:51 +0900

    add files for packaging

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open('VERSION') as f:
+    VERSION = f.read().strip()
+
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
+with open('requirements.txt') as f:
+    INSTALL_REQUIRES = [line.strip() for line in f if line.strip()]
+
+setup(
+    name='sqlcsv',
+    version=VERSION,
+    license='MIT',
+    description='Import/Export data to/from SQL database with CSV',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/yubessy/sqlcsv',
+    author='Shotaro Tanaka',
+    author_email='yubessy0@gmail.com',
+    packages=[
+        'sqlcsv',
+    ],
+    install_requires=INSTALL_REQUIRES,
+)
